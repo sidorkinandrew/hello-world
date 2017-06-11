686831f9eb3e8d1b74e9068432febcd210391331
dyvosvit <dyvosvit.ukraine@gmail.com>
2017-06-11 12:09:49 +0000

    Create mysetup.sh

---
+++ mysetup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#!/bin/bash
+GB_FDRNAME="Gunbot3.3.2"
+GB_FLN_NAME="GUNBOT_v3.3.2_Poloniex_Bittrex_Patch"
+printText () {
+  echo " ============================================================"
+  echo " $1"
+  echo " ============================================================"
+}
+printText "Setup of "+$GB_FDRNAME
