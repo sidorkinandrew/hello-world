df8aebe2a42ebeb5813947b996db7737bd76e277
dyvosvit <dyvosvit.ukraine@gmail.com>
2017-07-08 23:33:40 +0000

    Update fock.sh
    curl

--- fock.sh
+++ fock.sh
@@ -10,4 +10,4 @@ TMPLDR="/opt/unzip-tmp"
 GB_FDRNAME="Gunbot3.3.2"
 GB_FLNAME="GUNBOT_v3.3.2_Poloniex_Bittrex_Patch"
 MYPAIRS="currencies.txt"
-https://raw.githubusercontent.com/dyvosvit/hello-world/master/mypairs > /opt/gunbot/cfg/$MYPAIRS
+curl https://raw.githubusercontent.com/dyvosvit/hello-world/master/mypairs > /opt/gunbot/cfg/$MYPAIRS
