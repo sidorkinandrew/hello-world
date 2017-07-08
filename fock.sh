5c3d9333c34fc0dd291c30cb2c84c4166b985616
dyvosvit <dyvosvit.ukraine@gmail.com>
2017-07-08 23:32:45 +0000

    Update fock.sh
    MYPAIRS

--- fock.sh
+++ fock.sh
@@ -10,29 +10,4 @@ TMPLDR="/opt/unzip-tmp"
 GB_FDRNAME="Gunbot3.3.2"
 GB_FLNAME="GUNBOT_v3.3.2_Poloniex_Bittrex_Patch"
 MYPAIRS="currencies.txt"
-cd /opt/
-sudo unzip $GB_FLNAME.zip -d $GB_FLNAME
-sudo rm /opt/gunbot
-sudo ln -s /opt/$GB_FLNAME /opt/gunbot
-sudo mkdir /opt/gunbot/cfg
-sudo rm /opt/gunbot/*.exe
-sudo rm /opt/gunbot/gunthy-macos
-sudo rm /opt/gunbot/gunthy-linuxx86
-#sudo rm /opt/$GB_FLNAME.zip
-#sudo rm -R $TMPLDR
-sudo chmod +x /opt/gunbot/gunthy-linuxx64
-sudo echo "" > /opt/gunbot/cfg/$MYPAIRS
-sudo echo "" >> ~/.bashrc
-sudo echo "# GUNBOT's ALIASES" >> ~/.bashrc
-sudo echo "alias gbdir='cd /opt/gunbot'" >> ~/.bashrc
-sudo echo "alias gbmon='pm2 monit'" >> ~/.bashrc
-sudo echo "alias gbl='pm2 l'" >> ~/.bashrc
-sudo echo "alias gblog='pm2 logs'" >> ~/.bashrc
-sudo echo "alias gbstart='pm2 start'" >> ~/.bashrc
-sudo echo "alias gbstop='pm2 stop'" >> ~/.bashrc
-sudo echo "alias editpairs='mcedit /opt/gunbot/cfg/currencies.txt'" >> ~/.bashrc
-sudo mkdir /root/.pm2 -p
-sudo chmod g+rwx /root/.pm2
-sudo chmod g+rw /root/.pm2/*
-# editing currencies
-sudo echo 'SELECTED_EDITOR="/usr/bin/mcedit"'>~/.selected_editor
+https://raw.githubusercontent.com/dyvosvit/hello-world/master/mypairs > /opt/gunbot/cfg/$MYPAIRS
