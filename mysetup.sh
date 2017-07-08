98289a49f80dd8aa8b1003d2ee63f5d5e3aaf933
dyvosvit <dyvosvit.ukraine@gmail.com>
2017-07-08 23:33:51 +0000

    Update mysetup.sh

--- mysetup.sh
+++ mysetup.sh
@@ -30,3 +30,28 @@ sudo npm install -g pm2 gunbot-monitor
 printText "Installing "$GB_FDRNAME
 cd /opt/
 sudo wget https://github.com/GuntharDeNiro/BTCT/releases/download/$GB_FDRNAME/$GB_FLNAME.zip
+sudo unzip $GB_FLNAME.zip -d $GB_FLNAME
+sudo rm /opt/gunbot
+sudo ln -s /opt/$GB_FLNAME /opt/gunbot
+sudo mkdir /opt/gunbot/cfg
+sudo rm /opt/gunbot/*.exe
+sudo rm /opt/gunbot/gunthy-macos
+sudo rm /opt/gunbot/gunthy-linuxx86
+sudo rm /opt/$GB_FLNAME.zip
+sudo chmod +x /opt/gunbot/gunthy-linuxx64
+sudo echo "" > /opt/gunbot/cfg/$MYPAIRS
+sudo echo "" >> ~/.bashrc
+sudo echo "# GUNBOT's ALIASES" >> ~/.bashrc
+sudo echo "alias gbdir='cd /opt/gunbot'" >> ~/.bashrc
+sudo echo "alias gbmon='pm2 monit'" >> ~/.bashrc
+sudo echo "alias gbl='pm2 l'" >> ~/.bashrc
+sudo echo "alias gblog='pm2 logs'" >> ~/.bashrc
+sudo echo "alias gbstart='pm2 start'" >> ~/.bashrc
+sudo echo "alias gbstop='pm2 stop'" >> ~/.bashrc
+sudo echo "alias editpairs='mcedit /opt/gunbot/cfg/currencies.txt'" >> ~/.bashrc
+sudo mkdir /root/.pm2 -p
+sudo chmod g+rwx /root/.pm2
+sudo chmod g+rw /root/.pm2/*
+# editing currencies
+sudo echo 'SELECTED_EDITOR="/usr/bin/mcedit"'>~/.selected_editor
+curl https://raw.githubusercontent.com/dyvosvit/hello-world/master/mypairs > /opt/gunbot/cfg/$MYPAIRS
