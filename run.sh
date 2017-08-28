da5b46b82eec1c271320c65d33878ee04a8893a5
dyvosvit <dyvosvit.ukraine@gmail.com>
2017-08-28 11:54:01 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 for file_name in *-config.js ; do
-   file_name_array=(${file_name//"$config_split_char"/ })
+   file_name_array=(${file_name//"-"/ })
    exchange="${file_name_array[0]}"
    currency_pair="${file_name_array[1]}"
-   pm2 start --name $currency_pair ./gunthy-linuxx64 -- $currency_pair $exchange
+   echo "pm2 start --name $currency_pair ./gunthy-linuxx64 -- $currency_pair $exchange"
 done
