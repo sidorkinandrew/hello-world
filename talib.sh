6f25a6b186fc8f66a675e2388cdc7d06efbb89a6
dyvosvit <dyvosvit.ukraine@gmail.com>
2018-05-25 19:55:40 +0000

    Update talib.sh

--- talib.sh
+++ talib.sh
@@ -35,3 +35,9 @@ sudo make install
 sudo pip install cython
 sudo apt-get install python-dev -y
 sudo pip install TA-lib
+sudo fallocate -l 2G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
