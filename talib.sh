6dd4d7c2545d2b14cd6389640af007ad98fb8c0c
dyvosvit <dyvosvit.ukraine@gmail.com>
2018-05-25 20:01:34 +0000

    Update talib.sh

--- talib.sh
+++ talib.sh
@@ -41,3 +41,4 @@ sudo mkswap /swapfile
 sudo swapon /swapfile
 sudo cp /etc/fstab /etc/fstab.bak
 echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+sudo curl https://rclone.org/install.sh | sudo bash
