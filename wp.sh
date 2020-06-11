c82d203ed90ece1301f8bd17f2a6078a1c900cc2
dyvosvit <dyvosvit.ukraine@gmail.com>
2020-06-11 07:50:09 +0000

    Create wp.sh

---
+++ wp.sh
@@ -0,0 +1,5 @@
+wget https://www.apachefriends.org/xampp-files/7.4.6/xampp-linux-x64-7.4.6-0-installer.run
+chmod +x xampp-linux-x64-7.4.6-0-installer.run
+wget https://bitnami.com/redirect/to/1013270/bitnami-wordpress-5.4.1-0-module-linux-x64-installer.run
+chmod +x bitnami-wordpress-5.4.1-0-module-linux-x64-installer.run
+sudo ln -s /opt/lampp/lampp /etc/init.d/lampp
